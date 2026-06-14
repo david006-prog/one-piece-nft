07130ddaeb15db426db5a78debd0561d71c04562
david006-prog <idowuadedolapo20@gmail.com>
2026-06-14 03:07:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 
 export default defineConfig({
-  plugins: [tanstackStart(), nitro(), viteReact()],
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+  ],
 });
